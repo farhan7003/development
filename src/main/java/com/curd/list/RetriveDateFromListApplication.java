f7d69e6ef960160d266df67c8846ab15c9415033
farhan7003 <61114446+farhan7003@users.noreply.github.com>
2020-02-16 23:29:54 +0530

    test

--- src/main/java/com/curd/list/RetriveDateFromListApplication.java
+++ src/main/java/com/curd/list/RetriveDateFromListApplication.java
@@ -8,10 +8,12 @@ public class RetriveDateFromListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RetriveDateFromListApplication.class, args);
-		System.out.println("Its A git Try");
+		//System.out.println("Its A git Try");
 		System.out.println("I am Another Try For Git");
 		System.out.println("abhay");
 		System.out.println("farhan");
+		System.out.println("hyd");
+		
 	}
 
 }
