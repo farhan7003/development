c5dd3bc0fa665d4d48685c5033a2198a6271ef49
farhan <mdfarhan8961@gmail.com>
2020-02-16 22:52:16 +0530

    New Commit

--- src/main/java/com/curd/list/RetriveDateFromListApplication.java
+++ src/main/java/com/curd/list/RetriveDateFromListApplication.java
@@ -9,7 +9,7 @@ public class RetriveDateFromListApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RetriveDateFromListApplication.class, args);
 		//System.out.println("Its A git Try");
-		//System.out.println("I am Another Try For Git");
+		System.out.println("I am Another Try For Git");
 		System.out.println("abhay");
 		System.out.println("farhan");
 	}
