e2c11e8c6511ec4e5900c9392f2da5cde844b22d
farhan <mdfarhan8961@gmail.com>
2020-02-16 19:21:30 +0530

    This is Something wrong

---
+++ src/main/java/com/curd/list/RetriveDateFromListApplication.java
@@ -0,0 +1,13 @@
+package com.curd.list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetriveDateFromListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetriveDateFromListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curd/list/RetriveDateFromListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curd.list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetriveDateFromListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
